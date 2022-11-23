c05fa7f2597ca7c55458e735f9e8b1a40f7aa398
pabloandrestorres1 <pablooat1@gmail.com>
2022-11-23 20:18:27 +0100

    Commit 1

---
+++ 01ModulosCore/00-basics.js
@@ -0,0 +1,10 @@
+/*
+Esto es un primer ejemplo de un archivo
+ejecutado desde node
+*/
+console.log('Hola Mundo desde Node.js, esto se verá en el terminal de comandos')
+console.log(2 + 5)
+//console.log(DAW)
+setInterval(function (){
+   console.log('Hola Node.js')
+}, 1000)
